5cc34104c908a1a8de5c0e61892c0a6fc864c503
Panchem <hiebert-c62@webmail.uwinnipeg.ca>
2016-04-15 09:26:02 -0500

    fireworks

--- Assets/Scripts/ShellWeapon.cs
+++ Assets/Scripts/ShellWeapon.cs
@@ -5,13 +5,21 @@ public class ShellWeapon : MonoBehaviour {
 
     [SerializeField]
     private GameObject _shellPrefab;
-	
-	void Update () {
-	    if(Input.GetMouseButtonDown(0))
+
+    [SerializeField]
+    private GameObject _fireworks;
+
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
         {
             var shell = Instantiate(_shellPrefab, transform.position, transform.LookQuaternion(Camera.main.ScreenToWorldPoint(Input.mousePosition))) as GameObject;
 
             Physics2D.IgnoreCollision(shell.GetComponent<CircleCollider2D>(), GetComponent<BoxCollider2D>());
+        }
+
+        if(Input.GetMouseButtonDown(1))
+        {
+            Destroy(Instantiate(_fireworks, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity), 5f);
         }
 	}
 }
