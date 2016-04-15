afdc1c91ab20b857f15d84b5112e37c271aee0ac
Panchem <hiebert-c62@webmail.uwinnipeg.ca>
2016-04-15 10:57:29 -0500

    more fireworks

---
+++ Assets/FireworkChain.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireworkChain : MonoBehaviour {
+
+    public float t;
+    public int num;
+    public float dist;
+    public GameObject prefab;
+
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        for (int i = -num; i < num; i++)
+        {
+            var newFirework = Instantiate(prefab);
+
+            newFirework.transform.SetParent(transform, false);
+
+            newFirework.transform.position = new Vector3(dist * i, transform.position.y, 5);
+
+            var sys = newFirework.GetComponent<ParticleSystem>();
+
+            yield return new WaitForSeconds(t);
+        }
+    }
+}
--- Assets/Scripts/ShellWeapon.cs
+++ Assets/Scripts/ShellWeapon.cs
@@ -4,22 +4,36 @@ using System.Collections;
 public class ShellWeapon : MonoBehaviour {
 
     [SerializeField]
+    float lowness;
+
+    [SerializeField]
     private GameObject _shellPrefab;
 
     [SerializeField]
-    private GameObject _fireworks;
+    private GameObject[] _fireworks;
+
+    [SerializeField]
+    private GameObject[] _lowFireworks;
 
     void Update () {
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
         if (Input.GetMouseButtonDown(0))
         {
-            var shell = Instantiate(_shellPrefab, transform.position, transform.LookQuaternion(Camera.main.ScreenToWorldPoint(Input.mousePosition))) as GameObject;
+            var shell = Instantiate(_shellPrefab, transform.position, transform.LookQuaternion(mousePos)) as GameObject;
 
             Physics2D.IgnoreCollision(shell.GetComponent<CircleCollider2D>(), GetComponent<BoxCollider2D>());
         }
 
         if(Input.GetMouseButtonDown(1))
         {
-            Destroy(Instantiate(_fireworks, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity), 5f);
+            if(mousePos.y < -lowness)
+            {
+                Destroy(Instantiate(_lowFireworks[Random.Range(0, _lowFireworks.Length)], mousePos, Quaternion.identity), 20f);
+            } else
+            {
+                Destroy(Instantiate(_fireworks[Random.Range(0, _fireworks.Length)], mousePos, Quaternion.identity), 20f);
+            }
         }
 	}
 }
